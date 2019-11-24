b47ff6665f1fb4b52d8a404490dd83c957b4f4a0
bikov <bikov95@gmail.com>
2019-11-24 14:51:00 +0200

    feat(slider): add rails for all videos
    working with mock for now

--- src/components/compound-slider/slider-rail.js
+++ src/components/compound-slider/slider-rail.js
@@ -9,23 +9,36 @@ const railOuterStyle = {
     cursor: 'pointer',
 };
 
-const railInnerStyle = {
+const getRailInnerStyle = (order, start, length, color) => ({
     position: 'absolute',
-    width: '100%',
+    left: `${start}%`,
+    width: `${length}%`,
     height: 8,
+    bottom: 10 * (order - 1) + 4,
     transform: 'translate(0%, -50%)',
     borderRadius: 4,
     pointerEvents: 'none',
-    backgroundColor: 'rgb(155,155,155)',
-};
+    backgroundColor: color,
+});
 
-export const SliderRail = ({getRailProps}) => (
+export const SliderRail = ({getRailProps, order, start, length, color}) => (
     <>
         <div style={railOuterStyle} {...getRailProps()} />
-        <div style={railInnerStyle}/>
+        <div style={getRailInnerStyle(order, start, length, color)}/>
     </>
 );
 
 SliderRail.propTypes = {
     getRailProps: PropTypes.func.isRequired,
+    order: PropTypes.number,
+    start: PropTypes.number,
+    length: PropTypes.number,
+    color: PropTypes.string,
+};
+
+SliderRail.defaultProps = {
+    order: 0,
+    start: 0,
+    length: 100,
+    color: 'grey',
 };
